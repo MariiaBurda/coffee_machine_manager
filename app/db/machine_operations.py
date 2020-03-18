4aeec5a080ccc5083c54cf76c2019e96057f2521
Mariia Burda <mariiaburda93@gmail.com>
2020-03-18 13:40:27 +0200

    added get current state and change current state functionality

--- app/db/machine_operations.py
+++ app/db/machine_operations.py
@@ -8,10 +8,40 @@ def fill_resources(machine_id):
     cursor = db.cursor()
 
     sql = "UPDATE machine " \
-          "SET current_water_ml = max_water_ml, current_milk_ml = max_milk_ml, current_coffee_gr = max_coffee_gr " \
+          "SET current_water_ml = max_water_ml, " \
+          "current_milk_ml = max_milk_ml, " \
+          "current_coffee_gr = max_coffee_gr " \
           "WHERE id = %s"
     data = (machine_id,)
     cursor.execute(sql, data)
 
     db.commit()
 
+
+def get_current_state(machine_id):
+    db = mysql.connector.Connect(**config)
+    cursor = db.cursor()
+
+    sql = "SELECT id, name, current_water_ml, current_milk_ml, current_coffee_gr " \
+          "FROM machine " \
+          "WHERE id = %s"
+    data = (machine_id,)
+    cursor.execute(sql, data)
+    row = cursor.fetchone()
+
+    return row
+
+
+def change_current_state(machine_id, water_ml, milk_ml, coffee_gr):
+    db = mysql.connector.Connect(**config)
+    cursor = db.cursor()
+
+    sql = "UPDATE machine " \
+          "SET current_water_ml = current_water_ml - %s, " \
+          "current_milk_ml = current_milk_ml - %s, " \
+          "current_coffee_gr = current_coffee_gr - %s " \
+          "WHERE id = %s"
+    data = (water_ml, milk_ml, coffee_gr, machine_id)
+    cursor.execute(sql, data)
+
+    db.commit()
