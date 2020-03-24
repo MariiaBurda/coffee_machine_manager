ec7ac8536ac8c1cc55a235387eab7f58aa73110c
Mariia Burda <mariiaburda93@gmail.com>
2020-03-24 20:23:16 +0200

    Cleaned up the code

--- app/db/interface_connector.py
+++ app/db/interface_connector.py
@@ -3,17 +3,14 @@ import mysql.connector
 
 class InterfaceConnector:
     def __init__(self, db_config):
-        print('__init__ called')
         self.config_for_db = db_config
 
     def __enter__(self):
-        print('__enter__ called')
         self.db = mysql.connector.Connect(**self.config_for_db)
         self.cursor = self.db.cursor()
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        print('__exit__ called')
         if exc_type:
             print(f'exc_type: {exc_type}')
             print(f'exc_value: {exc_value}')
