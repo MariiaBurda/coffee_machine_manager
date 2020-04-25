0f0194de0959702e80e4457b92521ec203c54d2e
Mariia Burda <mariiaburda93@gmail.com>
2020-04-25 18:36:27 +0300

    Refactored sql code for more readability

--- app/database/initialization/sqlite/sqlite_coffee_machine_db_seed.py
+++ app/database/initialization/sqlite/sqlite_coffee_machine_db_seed.py
@@ -6,21 +6,26 @@ def sqlite_db_seed(db):
     cursor = conn.cursor()
 
     data_list = [
-        ("""
-        INSERT INTO machine(id, name, max_water_ml, max_milk_ml, max_coffee_gr, current_water_ml, current_milk_ml, current_coffee_gr)
-        VALUES
-            (1, 'Lazy coffee', 2000, 500, 1000, 2000, 500, 1000);"""),
-
-        ("""
-        INSERT INTO receipt(id, name, water_ml, milk_ml, coffee_gr)
-        VALUES
-            (1, 'Americano', 50, 0, 20),
-            (2, 'Americano with milk', 50, 10, 20),
-            (3, 'Latte', 30, 30, 10);"""),
-        ("""
-        INSERT INTO machine_receipt(machine_id, receipt_id)
-        VALUES
-            (1, 1), (1, 2), (1, 3);""")]
+        (
+            """INSERT INTO machine(id, name,         max_water_ml, max_milk_ml, max_coffee_gr, current_water_ml, current_milk_ml, current_coffee_gr)
+                VALUES            (1, 'Lazy coffee', 2000,         500,         1000,          2000,             500,             1000);
+            """
+        ),
+        (
+            """INSERT INTO receipt(id, name,                 water_ml, milk_ml, coffee_gr)
+               VALUES             (1, 'Americano',           50,       0,       20),
+                                  (2, 'Americano with milk', 50,       10,      20),
+                                  (3, 'Latte',               30,       30,      10);
+        """
+        ),
+        (
+            """INSERT INTO machine_receipt(machine_id, receipt_id)
+               VALUES                     (1,          1), 
+                                          (1,          2), 
+                                          (1,          3);
+            """
+         )
+    ]
 
     for data in data_list:
         cursor.execute(data)
