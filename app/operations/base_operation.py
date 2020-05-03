5c9ade1a36896f22f79bcb186b4553ee70e821a0
Mariia Burda <mariiaburda93@gmail.com>
2020-05-03 12:52:45 +0300

    Fixed config import bug, that happen when you run project after download from github

--- app/operations/base_operation.py
+++ app/operations/base_operation.py
@@ -1,7 +1,6 @@
 from .db_helper import DbHelper
 from .interface_connector import MySQLConnector, SQLiteConnector
 from .base_mapper import MySQLMapper, SQLiteMapper
-from .config import config_for_db
 
 
 class BaseOperation:
@@ -13,6 +12,7 @@ class BaseOperation:
     def initialize(self):
         db_sys = DbHelper.get_db_system()
         if db_sys == 'mysql':
+            from .config import config_for_db
             self.connector = MySQLConnector(config_for_db)
             self.mapper = MySQLMapper()
         elif db_sys == 'sqlite':
