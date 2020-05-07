54c767edff5cafdf54244b2dc0e2a701a97d29f4
Mariia Burda <mariiaburda93@gmail.com>
2020-05-07 18:12:12 +0300

    Refactored sql code for more readability

--- app/database/initialization/mysql/mysql_coffee_machine_db_init.sql
+++ app/database/initialization/mysql/mysql_coffee_machine_db_init.sql
@@ -2,41 +2,41 @@ CREATE DATABASE coffee_machine_manager;
 USE coffee_machine_manager;
 
 CREATE TABLE receipt(
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(20),
-water_ml INT, 
-milk_ml INT,
-coffee_gr INT
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    water_ml INT,
+    milk_ml INT,
+    coffee_gr INT
 );
 
 CREATE TABLE machine(
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(20),
-max_water_ml INT, 
-max_milk_ml INT,
-max_coffee_gr INT,
-current_water_ml INT, 
-current_milk_ml INT,
-current_coffee_gr INT
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    max_water_ml INT,
+    max_milk_ml INT,
+    max_coffee_gr INT,
+    current_water_ml INT,
+    current_milk_ml INT,
+    current_coffee_gr INT
 );
 
 CREATE TABLE machine_receipt(
-machine_id INT,
-receipt_id INT,
-PRIMARY KEY(machine_id,receipt_id),
-   FOREIGN KEY(machine_id) 
-       REFERENCES machine(id),
-   FOREIGN KEY(receipt_id) 
-       REFERENCES receipt(id)
+    machine_id INT,
+    receipt_id INT,
+    PRIMARY KEY(machine_id,receipt_id),
+       FOREIGN KEY(machine_id)
+           REFERENCES machine(id),
+       FOREIGN KEY(receipt_id)
+           REFERENCES receipt(id)
 ); 
 
 CREATE TABLE history(
-id INT AUTO_INCREMENT PRIMARY KEY,
-machine_id INT,
-receipt_id INT,
-date DATETIME,
-   FOREIGN KEY(machine_id) 
-       REFERENCES machine(id),
-   FOREIGN KEY(receipt_id) 
-       REFERENCES receipt(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    machine_id INT,
+    receipt_id INT,
+    date DATETIME,
+       FOREIGN KEY(machine_id)
+           REFERENCES machine(id),
+       FOREIGN KEY(receipt_id)
+           REFERENCES receipt(id)
 );
--- app/database/initialization/mysql/mysql_coffee_machine_db_seed.sql
+++ app/database/initialization/mysql/mysql_coffee_machine_db_seed.sql
@@ -2,21 +2,20 @@ USE coffee_machine_manager;
 
 -- insert data to machine
 
-INSERT INTO machine(name, max_water_ml, max_milk_ml, max_coffee_gr, current_water_ml, current_milk_ml, current_coffee_gr)
-VALUES
-    ('Lazy coffee', 2000, 500, 1000, 2000, 500, 1000);
+INSERT INTO machine (name,          max_water_ml, max_milk_ml, max_coffee_gr, current_water_ml, current_milk_ml, current_coffee_gr)
+VALUES              ('Lazy coffee', 2000,         500,         1000,          2000,             500,             1000);
 
 -- insert data to receipt
 
-INSERT INTO receipt(name, water_ml, milk_ml, coffee_gr)
-VALUES
-	('Americano', 50, 0, 20),
-    ('Americano with milk', 50, 10, 20),
-    ('Latte', 30, 30, 10);
+INSERT INTO receipt (name,                  water_ml, milk_ml, coffee_gr)
+VALUES              ('Americano',           50,       0,       20),
+                    ('Americano with milk', 50,       10,      20),
+                    ('Latte',               30,       30,      10);
     
--- added receipts to machine
+-- add receipts to machine
 
 INSERT INTO machine_receipt(machine_id, receipt_id)
-VALUES
-	(1, 1), (1, 2), (1, 3);
+VALUES                     (1,          1),
+                           (1,          2),
+                           (1,          3);
     
